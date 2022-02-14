1416bc01be042b9efee929a90afc62777a8b5226
paul-blackwell <paul@paulblackwell.io>
2022-02-14 21:46:04 +0000

    Set up scene in lesson 3

--- chapter-01/03-basic-scene/script.js
+++ chapter-01/03-basic-scene/script.js
@@ -1,1 +1,25 @@
-console.log('Hello three.js');
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0xff000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+
+// Get canvas from the DOM
+const canvas = document.querySelector('.webgl');
+console.log(canvas)
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
