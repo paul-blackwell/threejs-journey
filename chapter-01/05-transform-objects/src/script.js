377c09909cc15b22e17bf74beebc616df503afef
paul-blackwell <paul@paulblackwell.io>
2022-02-16 08:09:11 +0000

    Set up lession 4 and added axesHelper to the scene

---
+++ chapter-01/05-transform-objects/src/script.js
@@ -0,0 +1,59 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+
+// Update all axis at once
+mesh.position.set(0.7, - 0.6, 1)
+
+
+scene.add(mesh)
+
+// This will set our position length to one (position length is the value between our mesh and the center of the scene)
+// mesh.position.normalize();
+
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// This will get the distance between the mesh and our camera
+// console.log(mesh.position.distanceTo(camera.position))
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
