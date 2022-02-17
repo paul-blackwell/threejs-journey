fca98b75431a4959b5c72a0104388a25dfc32866
paul-blackwell <paul@paulblackwell.io>
2022-02-17 07:58:18 +0000

    Added requestAnimationFrame and setup deltaTime in lesson 6

--- chapter-01/06-animations/src/script.js
+++ chapter-01/06-animations/src/script.js
@@ -29,4 +29,26 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Time 
+let time = Date.now();
+
+// Animations
+const tick = () => {
+
+    // Time
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+    console.log(deltaTime)
+
+    // Update objects
+    mesh.rotation.y -= 0.001 * deltaTime
+
+    // Render
+    renderer.render(scene, camera)   
+    window.requestAnimationFrame(tick)
+}
+
+//tick()
