a308e5b462df7b358ed92027c664265ae1475ab0
paul-blackwell <paul@paulblackwell.io>
2022-02-23 08:29:43 +0000

    Started working on the Debug UI lesson 10

--- chapter-01/10-debug-ui/src/script.js
+++ chapter-01/10-debug-ui/src/script.js
@@ -2,6 +2,12 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
 
 /**
  * Base
@@ -19,6 +25,18 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Debug
+// Can do it like this
+// gui.add(mesh.position, 'y', - 3, 3, 0.01);
+// gui.add(mesh.position, 'x', - 3, 3, 0.01);
+// gui.add(mesh.position, 'z', - 3, 3, 0.01);
+gui
+    .add(mesh.position, 'y')
+    .min(-3).max(3)
+    .step(0.01);
+
+
 
 /**
  * Sizes
