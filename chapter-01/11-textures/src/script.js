3d0926b471876c3c2e8bbc3542cf1e0b5253fc56
paul-blackwell <paul@paulblackwell.io>
2022-02-27 08:10:31 +0000

    Added transformations to a texture in the scene lesson 11

--- chapter-01/11-textures/src/script.js
+++ chapter-01/11-textures/src/script.js
@@ -28,17 +28,19 @@ const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclus
 const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-colorTexture.repeat.x = 2
-colorTexture.repeat.y = 3
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
 // colorTexture.wrapS = THREE.RepeatWrapping
 // colorTexture.wrapT = THREE.RepeatWrapping
-colorTexture.wrapS = THREE.MirroredRepeatWrapping
-colorTexture.wrapT = THREE.MirroredRepeatWrapping
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
 
-colorTexture.offset.x = 0.5
-colorTexture.offset.y = 0.5
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
 
-colorTexture.rotation = Math.PI * 0.25  
+colorTexture.rotation = Math.PI * 0.25
+colorTexture.center.x = 0.5
+colorTexture.center.y = 0.5
 
 /**
  * Base
