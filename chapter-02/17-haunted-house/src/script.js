4831c099000450707658fd60a37fc03f62977541
paul-blackwell <paul@paulblackwell.io>
2022-03-14 08:27:52 +0000

    Started working on the graves lesson 17

--- chapter-02/17-haunted-house/src/script.js
+++ chapter-02/17-haunted-house/src/script.js
@@ -45,6 +45,55 @@ roof.position.y = 2.5 + 0.5
 roof.rotation.y = Math.PI * 0.25
 house.add(roof)
 
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2, 2),
+    new THREE.MeshStandardMaterial({color: '#aa7b7b'})
+)
+door.position.y = 1
+door.position.z =  2 + 0.01
+house.add(door)
+
+// Bushes
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854'})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
+house.add(bush1, bush2, bush3, bush4)
+
+// Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' })
+
+for(let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2 // Random angle
+    const x = Math.sin(angle) * 5
+    const z = Math.cos(angle) * 5
+
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.set(x, 0, z)
+    graves.add(grave)
+}
+
+
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
