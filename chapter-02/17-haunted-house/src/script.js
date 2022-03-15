063832465a16f504f9866e892f08a4e7db2c6c4b
paul-blackwell <paul@paulblackwell.io>
2022-03-15 07:44:27 +0000

    Added door textures lesson 17

--- chapter-02/17-haunted-house/src/script.js
+++ chapter-02/17-haunted-house/src/script.js
@@ -24,6 +24,14 @@ scene.fog = fog
  */
 const textureLoader = new THREE.TextureLoader()
 
+const doorColorTexture = textureLoader.load('textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
 /**
  * House
  */
@@ -51,8 +59,22 @@ house.add(roof)
 
 // Door
 const door = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(2, 2),
-    new THREE.MeshStandardMaterial({color: '#aa7b7b'})
+    new THREE.PlaneBufferGeometry(2.2, 2.2, 100, 100),
+    new THREE.MeshStandardMaterial({
+        map: doorColorTexture,
+        transparent: true,
+        alphaMap: doorAlphaTexture,
+        aoMap: doorAmbientOcclusionTexture,
+        displacementMap: doorHeightTexture,
+        displacementScale: 0.1,
+        normalMap: doorNormalTexture,
+        metalnessMap: doorMetalnessTexture,
+        roughness: doorAlphaTexture,
+    })
+)
+door.geometry.setAttribute(
+    'uv2', 
+    new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
 )
 door.position.y = 1
 door.position.z =  2 + 0.01
@@ -177,6 +199,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837');
 
 /**
  * Animate
