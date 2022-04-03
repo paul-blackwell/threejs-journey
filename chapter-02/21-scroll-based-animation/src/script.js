12db82a2503064ee41aa35e3b61b0347c5758b9d
paul-blackwell <paul@paulblackwell.io>
2022-04-03 08:02:27 +0100

    Added easing to the camera group lesson 21

--- chapter-02/21-scroll-based-animation/src/script.js
+++ chapter-02/21-scroll-based-animation/src/script.js
@@ -148,18 +148,21 @@ window.addEventListener('mousemove', (event) => {
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
     // Animate camera
     camera.position.y = - scrollY / sizes.height * objectsDistance
 
     const parallaxX = cursor.x 
     const parallaxY = - cursor.y
-    cameraGroup.position.x = parallaxX
-    cameraGroup.position.y = parallaxY
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
     // Animate meshes
     for(const mesh of sectionMeshes) {
