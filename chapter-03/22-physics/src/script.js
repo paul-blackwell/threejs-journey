32f40c8c2c8a5e77e54eb6022563f6f904f52198
paul-blackwell <paul@paulblackwell.io>
2022-04-09 07:46:24 +0100

    Removed the concrete and plastic materials and added a default material

--- chapter-03/22-physics/src/script.js
+++ chapter-03/22-physics/src/script.js
@@ -47,22 +47,34 @@ world.gravity.set(0, -9.82, 0) // Gravity Y of -9.82 (gravity constant on earth)
  * one of them a name i.e 'concrete' but this isn't actually 
  * a concrete we have just named it that
  */
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
+// const concreteMaterial = new CANNON.Material('concrete')
+// const plasticMaterial = new CANNON.Material('plastic')
+const defaultMaterial = new CANNON.Material('concrete')
 
 /**
  * A ContactMaterial is a combination of two Materials and
  * how they should collide
  */
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
+// const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+//     concreteMaterial,
+//     plasticMaterial,
+//     {
+//         friction: 0.1,
+//         restitution: 0.7
+//     }
+// )
+// world.addContactMaterial(concretePlasticContactMaterial);
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
     {
         friction: 0.1,
         restitution: 0.7
     }
 )
-world.addContactMaterial(concretePlasticContactMaterial);
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
 
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)  // 0.5 is the radius and it needs to be the same as the SphereGeometry radius
@@ -71,7 +83,7 @@ const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
     shape: sphereShape,
-    material: plasticMaterial,
+    // material: plasticMaterial,
 })
 world.addBody(sphereBody)
 
@@ -81,7 +93,7 @@ world.addBody(sphereBody)
  */
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
-floorBody.material = concreteMaterial
+// floorBody.material = concreteMaterial
 floorBody.mass = 0
 floorBody.addShape(floorShape)
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(- 1, 0, 0), Math.PI * 0.5) // This rotates the floorBody
