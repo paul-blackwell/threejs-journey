38fc9055f595535f042ac1988caeb5297658e3fc
paul-blackwell <paul@paulblackwell.io>
2022-05-23 07:55:57 +0100

    Started work on the Environment class

--- chapter-03/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ chapter-03/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import Experience from "../Experience";
+import Environment from './Environment';
 
 export default class World {
   constructor() {
@@ -9,9 +10,12 @@ export default class World {
     // Test mesh
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({wireframe: true})
+      new THREE.MeshStandardMaterial()
     );
 
-    this.scene.add(testMesh)
+    this.scene.add(testMesh);
+
+    // Setup
+    this.environment = new Environment();
   }
 }
