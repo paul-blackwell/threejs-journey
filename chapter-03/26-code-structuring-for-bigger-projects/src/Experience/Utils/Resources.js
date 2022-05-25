d0561906d6f1280518a5aff1b3176e5feffd5546
paul-blackwell <paul@paulblackwell.io>
2022-05-25 07:50:15 +0100

    Finished the Resources class

--- chapter-03/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
+++ chapter-03/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
@@ -15,12 +15,53 @@ export default class Resources extends EventEmitter {
     this.loaded = 0;
 
     this.setLoaders();
+    this.startLoading();
   }
 
   setLoaders() {
     this.loaders = {};
     this.loaders.gltfLoader = new GLTFLoader();
     this.loaders.textureLoader = new THREE.TextureLoader();
-    this.loaders.cubeTextureLOader = new THREE.CubeTextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+  }
+
+  startLoading() {
+    // Load each source
+    for (const source of this.sources) {
+      if (source.type === 'gltfModel') {
+        this.loaders.gltfLoader.load(
+          source.path,
+          (file) => {
+            this.sourceLoaded(source, file);
+          }
+        )
+      }
+      if (source.type === 'texture') {
+        this.loaders.textureLoader.load(
+          source.path,
+          (file) => {
+            this.sourceLoaded(source, file);
+          }
+        )
+      }
+      if (source.type === 'cubeTexture') {
+        this.loaders.cubeTextureLoader.load(
+          source.path,
+          (file) => {
+            this.sourceLoaded(source, file);
+          }
+        )
+      }
+    }
+  }
+
+  sourceLoaded(source, file) {
+    this.items[source.name] = file;
+
+    this.loaded ++;
+
+    if(this.loaded === this.toLoad) {
+      this.trigger('ready');
+    }
   }
 }
