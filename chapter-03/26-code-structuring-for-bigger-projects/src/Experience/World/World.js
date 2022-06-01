09a09c3b1326021af32bc45641372dc200f8410a
paul-blackwell <paul@paulblackwell.io>
2022-06-01 07:58:51 +0100

    Added the Fox class lesson 26

--- chapter-03/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ chapter-03/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
 import Experience from "../Experience";
 import Environment from './Environment';
 import Floor from './Floor';
+import Fox from './Fox';
 
 export default class World {
   constructor() {
@@ -9,18 +9,12 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    // Test mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-    );
-
-    this.scene.add(testMesh);
-
+ 
     // Wait for resources
     this.resources.on('ready', () => {
       // Setup
       this.floor = new Floor();
+      this.fox = new Fox();
       this.environment = new Environment();
     })
   }
