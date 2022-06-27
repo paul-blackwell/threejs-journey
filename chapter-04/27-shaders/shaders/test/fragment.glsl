397189889cc08954e8e9dad2d74116af418fe855
paul-blackwell <paul@paulblackwell.io>
2022-06-27 07:37:52 +0100

    Covered varying lesson 27

--- chapter-04/27-shaders/shaders/test/fragment.glsl
+++ chapter-04/27-shaders/shaders/test/fragment.glsl
@@ -1,6 +1,8 @@
- precision mediump float;
+precision mediump float;
+
+varying float vRandom;
 
 void main()
 {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(1.0, vRandom, 1.0, 1.0);
 }
--- chapter-04/27-shaders/shaders/test/vertex.glsl
+++ chapter-04/27-shaders/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
+varying float vRandom;
+
 void main()
 {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -15,4 +17,6 @@ void main()
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
+
+  vRandom = aRandom;
 }
