e1334acd0ff64805f171641bc239ef7971a521a8
paul-blackwell <paul@paulblackwell.io>
2022-06-28 07:58:31 +0100

    Started work on add textures to the fragment lesson 37

--- chapter-04/27-shaders/shaders/test/fragment.glsl
+++ chapter-04/27-shaders/shaders/test/fragment.glsl
@@ -2,8 +2,12 @@ precision mediump float;
 
 // varying float vRandom;
 
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
 void main()
 {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  vec4 textureColor = texture2D(uTexture, );
+  gl_FragColor = vec4(uColor, 1.0);
   // gl_FragColor = vec4(1.0, vRandom, 1.0, 1.0);
 }
--- chapter-04/27-shaders/src/script.js
+++ chapter-04/27-shaders/src/script.js
@@ -21,6 +21,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag-french.jpg')
 
 /**
  * Test mesh
@@ -44,6 +45,8 @@ const material = new THREE.RawShaderMaterial({
     uniforms: {
         uFrequency: { value: new THREE.Vector2(10, 5) },
         uTime: { value: 0},
+        uColor: { value: new THREE.Color('orange')},
+        uTexture: { value: flagTexture }
     }
 })
 
@@ -52,6 +55,7 @@ gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name(
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2 / 3;
 scene.add(mesh)
 
 /**
