62d7111e4830dd14c904fb3fe0c993bbf7ba64a0
paul-blackwell <paul@paulblackwell.io>
2022-06-28 07:43:54 +0100

    Added an animtion to the vertex lesson 27

--- chapter-04/27-shaders/src/script.js
+++ chapter-04/27-shaders/src/script.js
@@ -42,7 +42,8 @@ const material = new THREE.RawShaderMaterial({
     fragmentShader: testFragmentShader,
     transparent: true,
     uniforms: {
-        uFrequency: { value: new THREE.Vector2(10, 5) }
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0},
     }
 })
 
@@ -105,6 +106,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
