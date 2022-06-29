bac0d96c140edd66353b21d71ae8022a5d81140f
paul-blackwell <paul@paulblackwell.io>
2022-06-29 07:58:56 +0100

    Added color variations to the fragment lesson 27

--- chapter-04/27-shaders/shaders/test/fragment.glsl
+++ chapter-04/27-shaders/shaders/test/fragment.glsl
@@ -5,10 +5,12 @@ uniform sampler2D uTexture;
 
 // varying float vRandom;
 varying vec2 vUv;
+varying float vElevation;
 
 void main()
 {
   vec4 textureColor = texture2D(uTexture, vUv);
+  textureColor.rgb *= vElevation * 2.0 + 0.5;
   gl_FragColor = textureColor;
   // gl_FragColor = vec4(1.0, vRandom, 1.0, 1.0);
 }
--- chapter-04/27-shaders/shaders/test/vertex.glsl
+++ chapter-04/27-shaders/shaders/test/vertex.glsl
@@ -10,6 +10,7 @@ attribute vec2 uv;
 attribute float aRandom;
 
 varying vec2 vUv;
+varying float vElevation;
 
 
 // varying float vRandom;
@@ -17,8 +18,11 @@ varying vec2 vUv;
 void main()
 {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  modelPosition.z += elevation;
   // modelPosition.z += aRandom * 0.1;
 
 
@@ -28,6 +32,7 @@ void main()
   gl_Position = projectedPosition;
 
   vUv = uv;
+  vElevation = elevation;
 
   // vRandom = aRandom;
 }
